42fe54e2bc83544583711006f54b81480b0aa3b7
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2025-06-28 08:09:01 +0500

    Add constant for school address and display it

--- 2-Constants.php
+++ 2-Constants.php
@@ -1,4 +1,7 @@
 <?php
 define("SCHOOL_NAME", "NAWAB ACADEMY");
 echo "Welcome to " . SCHOOL_NAME . "<br>";
+
+const SCHOOL_ADDRESS = "123 Academy Lane, Cityville";
+echo "Address: " . SCHOOL_ADDRESS . "<br>";
 ?>

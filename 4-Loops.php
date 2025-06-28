915be7ecb01588e838402493520397a3cb69e110
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2025-06-28 08:14:06 +0500

    4-Loops

---
+++ 4-Loops.php
@@ -0,0 +1,30 @@
+<?php
+$i = 1;
+
+while ($i <= 5) {
+    echo "Number: $i <br>";
+    $i++;
+}
+
+
+$j = 1;
+
+do {
+    echo "Count: $j <br>";
+    $j++;
+} while ($j <= 3);
+
+
+
+for ($x = 1; $x <= 5; $x++) {
+    echo "Value is: $x <br>";
+}
+
+
+$colors = ["Red", "Green", "Blue"];
+
+foreach ($colors as $color) {
+    echo "Color: $color <br>";
+}
+?>
+

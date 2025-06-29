fbe75c3414837f2860367f9b0d5fa6dd45f354a5
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2025-06-29 07:11:12 +0500

    Fix connection error handling in database connection script

--- 12-connection.php
+++ 12-connection.php
@@ -6,8 +6,9 @@ $database = "learnphp";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($conn = false){
+    die("Connection failed: ");
+}else {
+    echo "Connected successfully";
 }
-echo "Connected successfully";
 ?>
